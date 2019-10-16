ae58c6f3965fd75179d46a407024def8716fb7b0
Vijay Raghav Varada <vjvarada@fracktal.in>
2019-10-16 19:24:34 +0530

    added code to remove gcode scripts on restore factory defaults

--- octoprint_Julia2018ExtendedTouchUI/Main.py
+++ octoprint_Julia2018ExtendedTouchUI/Main.py
@@ -41,6 +41,7 @@ import logging
 import RPi.GPIO as GPIO
 
     #Update
+    #Update
 from collections import OrderedDict
 
 
@@ -1698,6 +1699,7 @@ class MainUiClass(QtGui.QMainWindow, mainGUI_extended.Ui_MainWindow):
             os.system('sudo cp -f config/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf')
             os.system('sudo rm -rf /home/pi/.octoprint/users.yaml')
             os.system('sudo rm -rf /home/pi/.octoprint/printerProfiles/*')
+            os.system('sudo rm -rf /home/pi/.octoprint/scripts/gcode')
             os.system('sudo cp -f config/config_Julia2018ExtendedTouchUI.yaml /home/pi/.octoprint/config.yaml')
             self.tellAndReboot("Settings restored. Rebooting...")
 
